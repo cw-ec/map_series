ca9d2a2b11ebb8efea9b3cdc22f5235aeaa427ef
chris w <christopher.wenkoff@elections.ca>
2024-03-22 11:38:05 -0400

    Resolved errors with new zipping function. All items in fed list now unique and no .pdf files being passed into that list either

--- toolbox/components/pdf_management.py
+++ toolbox/components/pdf_management.py
@@ -22,13 +22,15 @@ class MapPdfSort:
 
         def zip_folders(outdir, fedlist: list) -> None:
             """Zips all fed folders that were just processed, delete the old folder structure"""
-            for fed in fedlist:
+
+            for fed in list(set(fedlist)):
                 self.logger.info(f" Zipping: {fed}")
                 fed_path = Path(os.path.join(outdir, fed))
-                make_archive(str(fed_path), 'zip', root_dir=fed_path)
-                self.logger.info(f" Deleting original directory for {fed}")
                 if os.path.exists(fed_path):
-                    rmtree(fed_path)
+                    make_archive(str(fed_path), 'zip', root_dir=fed_path)
+                    self.logger.info(f" Deleting original directory for {fed}")
+                    if os.path.exists(fed_path):
+                        rmtree(fed_path)
 
         root = Path(self.ddir)
         # Iterate over the map pdf docs and sort them into the folder structure
@@ -36,7 +38,7 @@ class MapPdfSort:
         for file in root.glob("*.pdf"):
 
             ptype = file.name.split('_')[0]
-            fed = file.name.split('_')[1]
+            fed = file.name.split('_')[1].split('.')[0]
             if fed not in fed_list:
                 fed_list.append(fed)
 
@@ -61,6 +63,7 @@ class MapPdfSort:
                 Path(out_pdf_path).mkdir(parents=True, exist_ok=True)
                 self.logger.info(f"Sorting: {file.name}")
                 copyfile(os.path.join(root, file.name), os.path.join(out_pdf_path, f"{ptype}_{fed}_{suffix}.pdf"))
+
         self.logger.info("Zipping all dumped files")
         # Zip all the folders that were just processed
         zip_folders(outdir=self.sdir, fedlist=fed_list)
